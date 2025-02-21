344d9764e8c48480ad1668a1041cf241bcea0993
Malte Arnold <maltearnold@hotmail.de>
2025-02-21 15:04:38 +0100

    Create aufgabe5.cs

---
+++ Aufgabe 5/aufgabe5.cs
@@ -0,0 +1,12 @@
+string Wochentag(int zahl)
+{
+    List<string> tage = new List<string>{"Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"};
+    
+    string tag = tage[zahl - 1];
+    
+    return tag;
+}
+
+int input = 7;
+string tag = Wochentag(input);
+Console.WriteLine(tag);
